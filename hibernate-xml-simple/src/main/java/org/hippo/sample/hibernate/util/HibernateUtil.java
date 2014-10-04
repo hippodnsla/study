b2edbeceaf91ce6e5d43be9f086f158d70627ae7
hippodnsla <hippodnsla@yahoo.com>
2014-10-04 21:19:04 +0800

    project hibernate-xml-simple

---
+++ hibernate-xml-simple/src/main/java/org/hippo/sample/hibernate/util/HibernateUtil.java
@@ -0,0 +1,19 @@
+package org.hippo.sample.hibernate.util;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class HibernateUtil {
+	
+	private static SessionFactory sessionFactory;
+	
+	static {
+		sessionFactory = new Configuration().configure().buildSessionFactory();
+	}
+	
+	public static SessionFactory getSessionFactory() {
+		return sessionFactory;
+	}
+	
+	
+}
